b0e95102fcc7a39edea8c68ed8b7fd75dc1d4818
silencedracer360-collab <260802745+silencedracer360-collab@users.noreply.github.com>
2026-05-28 13:23:33 -0500

    Implementación Endpoints Crud Factura

--- main.py
+++ main.py
@@ -165,3 +165,45 @@ async def crear_factura(
     return factura_val
 
 
+@app.put("/facturas/{id}")
+async def editar_factura(
+    id: int,
+    datos_factura: EditarFactura
+):
+
+    for i, factura in enumerate(listas_facturas):
+
+        if factura.id == id:
+
+            factura_val = Factura.model_validate(
+                datos_factura.model_dump()
+            )
+
+            factura_val.id = id
+
+            listas_facturas[i] = factura_val
+
+            return {
+                "mensaje": "Factura actualizada",
+                "factura": factura_val
+            }
+
+    return {"error": "Factura no encontrada"}
+
+
+@app.delete("/facturas/{id}")
+async def eliminar_factura(id: int):
+
+    for i, factura in enumerate(listas_facturas):
+
+        if factura.id == id:
+
+            del listas_facturas[i]
+
+            return {
+                "mensaje": "Factura eliminada"
+            }
+
+    return {"error": "Factura no encontrada"}
+
+
